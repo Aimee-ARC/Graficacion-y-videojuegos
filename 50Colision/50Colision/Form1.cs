d53ebc3c517dadd53a3dbefb0fc0752030ce9a23
aimee ramirez <122326477+Aimee-ARC@users.noreply.github.com>
2023-02-23 22:21:02 -0600

    50 colition

---
+++ 50Colision/50Colision/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _50Colision
+{
+    public partial class Form1 : Form
+    {
+        List<Pelota> pelotas = new List<Pelota>();
+        Random rnd = new Random();
+        private int pelotasAgregadas = 0;
+        Bitmap bitmap;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bitmap);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            AgregarPelota();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pelotasAgregadas < 50)
+            {
+                AgregarPelota();
+                pelotasAgregadas++;
+            }
+            ActualizarPelotas();
+        }
+
+
+
+        private void AgregarPelota()
+        {
+            // Crear nueva pelota con valores aleatorios
+            Pelota pelota = new Pelota();
+            pelota.X = rnd.Next(pictureBox1.Width);
+            pelota.Y = rnd.Next(pictureBox1.Height);
+            pelota.VelocidadX = rnd.Next(30) - 5;
+            pelota.VelocidadY = rnd.Next(30) - 5;
+            pelota.Radio = rnd.Next(15, 25);
+
+            // Agregar pelota a la lista
+            pelotas.Add(pelota);
+        }
+
+        private void ActualizarPelotas()
+        {
+            // Limpiar dibujo anterior
+            g.Clear(Color.Black);
+
+            // Actualizar y dibujar cada pelota
+            foreach (Pelota pelota in pelotas)
+            {
+                pelota.Actualizar(pictureBox1.ClientRectangle, pelotas);
+
+                Brush brush = new SolidBrush(Color.FromArgb(226, 95, 190));
+                g.FillEllipse(brush, pelota.X - pelota.Radio, pelota.Y - pelota.Radio, pelota.Radio * 2, pelota.Radio * 2);
+            }
+
+            // Actualizar imagen del PictureBox
+            pictureBox1.Image = bitmap;
+        }
+
+        private void btnAgregarPelotas_Click(object sender, EventArgs e)
+        {
+            //esto era para agregar pelotas con un click
+        }
+    }
+
+   
+}
---
+++ 50Colision/50Colision/Pelota.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50Colision
+{
+    public class Pelota
+    {
+        public int X { get; set; } // posición horizontal
+        public int Y { get; set; } // posición vertical
+        public int VelocidadX { get; set; } // velocidad horizontal
+        public int VelocidadY { get; set; } // velocidad vertical
+        public int Radio { get; set; } // tamaño de la pelota
+
+
+
+        public void Actualizar(RectangleF limites, List<Pelota> pelotas)
+        {
+            // Actualizar posición
+            X += VelocidadX;
+            Y += VelocidadY;
+
+            // Comprobar colisiones con el borde de la ventana
+            if (X < limites.Left + Radio || X > limites.Right - Radio)
+            {
+                VelocidadX = -VelocidadX;
+            }
+            if (Y < limites.Top + Radio || Y > limites.Bottom - Radio)
+            {
+                VelocidadY = -VelocidadY;
+            }
+
+            // Comprobar colisiones con otras pelotas
+            foreach (Pelota otra in pelotas)
+            {
+                if (otra == this) continue;
+
+                int dx = otra.X - X;
+                int dy = otra.Y - Y;
+                int distancia = (int)Math.Sqrt(dx * dx + dy * dy);
+
+                if (distancia < Radio + otra.Radio)
+                {
+                    // Calcular nueva velocidad para ambas pelotas
+                    int nuevaVelocidadX = (VelocidadX * (Radio - otra.Radio) + (2 * otra.Radio * otra.VelocidadX)) / (Radio + otra.Radio);
+                    int nuevaVelocidadY = (VelocidadY * (Radio - otra.Radio) + (2 * otra.Radio * otra.VelocidadY)) / (Radio + otra.Radio);
+                    int otraNuevaVelocidadX = (otra.VelocidadX * (otra.Radio - Radio) + (2 * Radio * VelocidadX)) / (Radio + otra.Radio);
+                    int otraNuevaVelocidadY = (otra.VelocidadY * (otra.Radio - Radio) + (2 * Radio * VelocidadY)) / (Radio + otra.Radio);
+
+                    VelocidadX = nuevaVelocidadX;
+                    VelocidadY = nuevaVelocidadY;
+                    otra.VelocidadX = otraNuevaVelocidadX;
+                    otra.VelocidadY = otraNuevaVelocidadY;
+
+                    // Mover pelotas para que no se superpongan
+                    int distanciaMovimiento = Radio + otra.Radio - distancia;
+                    int movX = distanciaMovimiento * dx / distancia;
+                    int movY = distanciaMovimiento * dy / distancia;
+
+                    X -= movX;
+                    Y -= movY;
+                    otra.X += movX;
+                    otra.Y += movY;
+                }
+            }
+        }
+    }
+}
