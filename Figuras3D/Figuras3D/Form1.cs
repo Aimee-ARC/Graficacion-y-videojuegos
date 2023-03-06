5e51f5cb8657113fb3c5698416b1f936f740c2d2
aimee ramirez <122326477+Aimee-ARC@users.noreply.github.com>
2023-03-05 20:53:16 -0600

    Add files via upload

---
+++ Figuras3D/Figuras3D/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Figuras3D
+{
+    public partial class Form1 : Form
+    {
+        Canvas canvas;
+        bool x, y, z = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            canvas = new Canvas(PCT_CANVAS);
+            //canvas.Cubo();
+            //canvas.Icosahedro();
+            //canvas.Esfera();
+            canvas.Cono();
+            //canvas.Cilindro();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RotarX_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            x = true;
+            y = z = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (x)
+            {
+                y = z = false;
+                canvas.RotacionX();
+            }
+            else if (y)
+            {
+                x = z = false;
+                canvas.RotacionY();
+            }
+            else if (z)
+            {
+                x = y = false;
+                canvas.RotacionZ();
+            }
+
+        }
+
+        private void RotarY_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled= true;
+            y = true;
+            x = z = false;
+        }
+
+        private void RotarZ_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            z = true;
+            x = y =  false;
+        }
+    }
+}
